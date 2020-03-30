15b707ba045304d784e74da6c4bf9ff91216b497
kutar37 <kutar37@gmail.com>
2020-03-30 13:42:02 +0900

    removeEvents 에러 수정

--- js/editEvent.js
+++ js/editEvent.js
@@ -3,6 +3,8 @@
  * ************** */
 var editEvent = function (event, element, view) {
 
+    $('#deleteEvent').data('id', event._id); //클릭한 이벤트 ID
+
     $('.popover.fade.top').remove();
     $(element).popover("hide");
 
@@ -89,23 +91,23 @@ var editEvent = function (event, element, view) {
         });
 
     });
-
-    // 삭제버튼
-    $('#deleteEvent').on('click', function () {
-        $('#deleteEvent').unbind();
-        $("#calendar").fullCalendar('removeEvents', [event._id]);
-        eventModal.modal('hide');
-
-        //삭제시
-        $.ajax({
-            type: "get",
-            url: "",
-            data: {
-                //...
-            },
-            success: function (response) {
-                alert('삭제되었습니다.');
-            }
-        });
-    });
 };
+
+// 삭제버튼
+$('#deleteEvent').on('click', function () {
+    $("#calendar").fullCalendar('removeEvents', $(this).data('id'));
+    eventModal.modal('hide');
+
+    //삭제시
+    $.ajax({
+        type: "get",
+        url: "",
+        data: {
+            //...
+        },
+        success: function (response) {
+            alert('삭제되었습니다.');
+        }
+    });
+
+});
